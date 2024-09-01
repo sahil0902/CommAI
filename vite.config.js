410a99811d6ae82e89b21af1bf7ddd8ce6587524
Muhammad Sahil <muhammadsahil757@gmail.com>
2024-09-01 17:08:21 +0100

    improbed config.js

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Adjust this if your app is deployed in a subdirectory
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      target: 'es2020',
-    },
-  },
-  esbuild: {
-    logOverride: { 'this-is-undefined-in-esm': 'silent' }
-  },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './src/main.jsx',
-        polyfills: './src/polyfills.js',
-      },
-    },
-  },
-  server: {
-    
-    // to ensure it serves the index.html for all routes in development
-    historyApiFallback: true,
-  },
-});
+})
